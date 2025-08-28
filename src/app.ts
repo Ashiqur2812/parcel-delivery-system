1d5ea8b9df57dc4aa5cae020fbd157c65f837873
Ashiqur <ashiqur2812@gmail.com>
2025-08-28 16:44:45 +0600

    added AppError

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import morgan from 'morgan'
 import express, { Request, Response } from 'express';
 const app = express();
 
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev")); 
 
 app.get('/', async (req: Request, res: Response) => {
     res.status(200).json({
