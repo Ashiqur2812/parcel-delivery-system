7204c8dd8c36751a66668791538302fc6113a4f0
Ashiqur <ashiqur2812@gmail.com>
2025-08-29 12:16:30 +0600

    added the setCookie file

---
+++ src/app/utils/setCookie.ts
@@ -0,0 +1,27 @@
+import { Response } from "express";
+import { envVars } from "../config/env";
+
+export interface AuthToken {
+    accessToken?: string,
+    refreshToken?: string;
+}
+
+export const setAuthCookie = (res: Response, tokenInfo: AuthToken) => {
+    const isProduction = envVars.NODE_ENV === 'production';
+
+    if (tokenInfo.accessToken) {
+        res.cookie('accessToken', tokenInfo.accessToken, {
+            httpOnly: true,
+            secure: isProduction,
+            sameSite: 'strict'
+        });
+    }
+
+    if (tokenInfo.refreshToken) {
+        res.cookie('refreshToken', tokenInfo.refreshToken, {
+            httpOnly: true,
+            secure: isProduction,
+            sameSite: 'strict'
+        });
+    }
+};
