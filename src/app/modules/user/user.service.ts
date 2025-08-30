ee5ec5117bb2e065d79c366b75ed00d5c8e55667
Ashiqur <ashiqur2812@gmail.com>
2025-08-30 11:20:36 +0600

    update user service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,6 +4,7 @@ import { User } from "./user.model";
 import httpStatus from 'http-status-codes';
 import bcryptjs from 'bcryptjs';
 import config from '../../config/env';
+import { JwtPayload } from "jsonwebtoken";
 
 
 const createUser = async (payload: Partial<IUser>) => {
@@ -37,6 +38,35 @@ const createUser = async (payload: Partial<IUser>) => {
 
 };
 
+const updateUser = async (id: string, payload: Partial<IUser>, decodedToken: JwtPayload) => {
+    const existingUser = await User.findById(id);
+
+    if (!existingUser) {
+        throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+    }
+
+    const userId = decodedToken.id || decodedToken.userId;
+    // check ownerShip of admin privileges
+    if (userId !== id && userId.role !== Role.ADMIN) {
+        throw new AppError(httpStatus.FORBIDDEN, 'You can only update your own profile');
+    }
+
+    // only admin can change role and status
+    if (payload.role && decodedToken.role !== Role.ADMIN) {
+        throw new AppError(httpStatus.FORBIDDEN, 'Only admin can change status');
+    }
+
+    // Hash password
+    if (payload.password) {
+        payload.password = await bcryptjs.hash(payload.password, Number(config.BCRYPT_SALT_ROUND));
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, payload, { new: true, runValidators: true });
+
+    return updatedUser;
+};
+
 export const userServices = {
-    createUser
+    createUser,
+    updateUser
 };
