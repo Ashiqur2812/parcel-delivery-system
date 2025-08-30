c37b52494f4a35b6a7c6d70a61ecb73a35b4261c
Ashiqur <ashiqur2812@gmail.com>
2025-08-30 11:01:48 +0600

    done the check user authentication functionality

--- src/app/middleware/checkUserOwnerOrAdmin.ts
+++ src/app/middleware/checkUserOwnerOrAdmin.ts
@@ -5,19 +5,31 @@ import { Role } from "../modules/user/user.interface";
 
 export const checkUserOwnerOrAdmin = (req: Request, res: Response, next: NextFunction) => {
     try {
-
         const { id } = req.params;
 
-        if (!req.user) {
+        if (!req.body) {
             throw new AppError(httpStatus.UNAUTHORIZED, 'User information not found');
         }
 
         // allow admin to access user
-        if (req.user.role === Role.ADMIN) {
+        if (req.body.role === Role.ADMIN) {
             return next();
         }
 
-        const userId = req.user.userId || req.user.id || req.user._id
+        const userId = req.body.userId || req.body.id || req.body._id;
+
+        if (!userId) {
+            throw new AppError(httpStatus.UNAUTHORIZED, 'User information is not found');
+        }
+
+        // allow user to access their own data
+        if (userId.toString() === id.toString()) {
+            return next();
+        }
+
+        throw new AppError(
+            httpStatus.FORBIDDEN, 'Access denied: You can only access your own resources'
+        );
 
     } catch (error) {
         next(error);
