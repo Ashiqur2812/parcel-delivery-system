6117bd1af51ddbd4980e57087faf66fbe3445585
Ashiqur <ashiqur2812@gmail.com>
2025-08-30 15:13:35 +0600

    added the create user zod schema

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Router } from "express";
+
+const router = Router()
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,43 @@
+import z from "zod";
+import { Role } from "./user.interface";
+
+export const createUserZodSchema = z.object({
+    name: z
+        .string({ message: 'Name must be string' })
+        .min(2, { message: 'Name must be 2 characters long' })
+        .max(50, { message: 'Name cannot exist more than 50 characters' }),
+
+    email: z.string().email({ message: 'Invalid email address' }),
+
+    password: z
+        .string({ message: 'password must be string' })
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .regex(/[A-Z]/, {
+            message: "Password must contain at least one uppercase letter",
+        })
+        .regex(/[0-9]/, {
+            message: "Password must contain at least one number",
+        })
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, {
+            message: "Password must contain at least one special character",
+        }),
+
+    phone: z
+        .string({ message: 'Phone number must be string' })
+        .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
+            message:
+                "Phone number must be valid for Bangladesh (e.g., +8801XXXXXXXXX or 01XXXXXXXXX)",
+        })
+        .optional(),
+
+    role: z.enum([Role.ADMIN, Role.SENDER, Role.RECEIVER]).default(Role.SENDER),
+
+    address: z
+        .object({
+            street: z.string().optional(),
+            city: z.string().optional(),
+            district: z.string().optional(),
+            postalCode: z.string().optional()
+        })
+
+});
