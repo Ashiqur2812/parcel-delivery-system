0c19f8786b01d40a94c9a818e6f9e253aa180264
Ashiqur <ashiqur2812@gmail.com>
2025-08-30 15:18:47 +0600

    added the update user zod schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { Role } from "./user.interface";
+import { Role, UserStatus } from "./user.interface";
 
 export const createUserZodSchema = z.object({
     name: z
@@ -39,5 +39,54 @@ export const createUserZodSchema = z.object({
             district: z.string().optional(),
             postalCode: z.string().optional()
         })
-
 });
+
+
+export const updateUserZodSchema = z.object({
+    name: z
+        .string({ message: 'Name must be string' })
+        .min(2, { message: 'Name must be 2 characters long' })
+        .max(50, { message: 'Name cannot exist more than 50 characters' })
+        .optional(),
+
+    email: z.string().email({ message: 'Invalid email address' }).optional(),
+
+    password: z
+        .string({ message: 'password must be string' })
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .regex(/[A-Z]/, {
+            message: "Password must contain at least one uppercase letter",
+        })
+        .regex(/[0-9]/, {
+            message: "Password must contain at least one number",
+        })
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, {
+            message: "Password must contain at least one special character",
+        })
+        .optional(),
+
+    phone: z
+        .string({ message: 'Phone number must be string' })
+        .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
+            message:
+                "Phone number must be valid for Bangladesh (e.g., +8801XXXXXXXXX or 01XXXXXXXXX)",
+        })
+        .optional(),
+
+    role: z
+        .enum([Role.ADMIN, Role.SENDER, Role.RECEIVER]).default(Role.SENDER).optional(),
+
+    status: z
+        .enum([UserStatus.ACTIVE, UserStatus.INACTIVE, UserStatus.BLOCKED])
+        .optional(),
+
+    address: z
+        .object({
+            street: z.string().optional(),
+            city: z.string().optional(),
+            district: z.string().optional(),
+            postalCode: z.string().optional()
+        })
+        .optional()
+});
+
