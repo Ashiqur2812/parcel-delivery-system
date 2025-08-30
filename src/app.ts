11e117b5a10b255e66bdd25960fba1c73ff17570
Ashiqur <ashiqur2812@gmail.com>
2025-08-31 00:09:09 +0600

    compleleted app.ts functionality

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,35 @@
 import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import morgan from 'morgan'
+import morgan from 'morgan';
+import expressSession from 'express-session';
+import config from './app/config/env';
+import passport from 'passport';
+import { router } from './app/routes';
+import { globalErrorHandler } from './app/middleware/globalErrorHandler';
+import notFound from './app/middleware/notFound';
 const app = express();
 
+app.use(
+    expressSession({
+        secret: config.EXPRESS_SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
-app.use(morgan("dev")); 
+app.use(morgan("dev"));
+
+// routes
+app.use('/api/v1', router);
 
 app.get('/', async (req: Request, res: Response) => {
     res.status(200).json({
@@ -15,5 +37,9 @@ app.get('/', async (req: Request, res: Response) => {
         message: 'Welcome to Parcel Management System'
     });
 });
+
+// error handler
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
