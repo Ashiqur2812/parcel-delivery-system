31db97d4056f45def8910c9e5b3610501d93de5a
Ashiqur <ashiqur2812@gmail.com>
2025-08-31 00:01:02 +0600

    completed the user route functionality

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,4 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Router } from "express";
+import express from 'express';
+import { validateRequest } from '../../middleware/validationRequest';
+import { blockUserZodSchema, createUserZodSchema, updateUserZodSchema } from './user.validation';
+import { UserController } from './user.controller';
+import { checkAuth } from '../../middleware/checkAuth';
+import { Role } from './user.interface';
+import { checkUserOwnerOrAdmin } from '../../middleware/checkUserOwnerOrAdmin';
 
-const router = Router()
+const router = express.Router();
+
+router.post('/register', validateRequest(createUserZodSchema), UserController.createUser);
+
+router.get('/all-users', checkAuth(Role.ADMIN), UserController.getAllUsers);
+
+router.patch('/block/:id', checkAuth(Role.ADMIN), validateRequest(blockUserZodSchema), UserController.blockUserController);
+
+router.delete('/:id', checkAuth(Role.ADMIN), UserController.deleteUser);
+
+router.patch('/:id', validateRequest(updateUserZodSchema), checkAuth(Role.ADMIN, Role.SENDER, Role.RECEIVER), checkUserOwnerOrAdmin, UserController.updateUser);
